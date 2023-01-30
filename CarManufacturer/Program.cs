e498937d1014d0cfae0158e48750d6cfd9467dd3
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-30 17:29:07 +0200

    Add project files.

---
+++ CarManufacturer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Engine treeLiterDiesel = new Engine(215, 3);
+            Tire[] tires = new Tire[4]
+            {
+                new Tire(2020, 2),
+                new Tire(2020, 2),
+                new Tire(2020, 2),
+                new Tire(2020, 2)
+            };
+            string make = "BMW";
+            string model = "X3";
+            int year = 2020;
+            double fuelQuantity = 100;
+            double fuelConsumption = 10;
+
+            Car car1 = new Car();
+            Car car2 = new Car(make, model,year);
+            Car car3 = new Car(make, model,year,fuelQuantity, fuelConsumption);
+            Car car4 = new Car(make, model, year, fuelQuantity, fuelConsumption, treeLiterDiesel, tires);
+
+        }
+    }
+}
